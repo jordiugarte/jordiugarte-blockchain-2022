aeab076be6fb607ad45593899b861c498cde2f00
Jordi Ugarte <jordi933@gmail.com>
2022-05-20 14:09:17 -0400

    20-05-22

--- Inbox.sol
+++ Inbox.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.0;
 contract Inbox {
 
     string public message;
+    address private ownerAdress;
 
     constructor(string memory initialMessage) {
+        ownerAdress = msg.sender;
         message = initialMessage;
     }
 
@@ -14,6 +16,7 @@ contract Inbox {
     }
 
     function setMessage(string memory newMessage) public {
+        require(msg.sender == ownerAdress, "Solo el owner puede modificar sus caracteristicas.");
         message = newMessage;
     }
 }
