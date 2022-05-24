5ceaf28e5f063d1606b77318aad3bfe734f292b4
mallkusoft <mallkusoft2022@gmail.com>
2022-05-24 13:34:36 -0400

    avance

--- Inbox.sol
+++ Inbox.sol
@@ -4,16 +4,27 @@ pragma solidity ^0.8.0;
 contract Inbox {
 
     string public message;
+    address private ownerAddress;
 
-    constructor() {
-        
+    constructor(string memory initialMessage) {
+        ownerAddress = msg.sender;
+        message = initialMessage;
     }
 
-    function getMessage() public view returns (string memory) {
+    function getMessage() public ownerRestricted(msg.sender) view returns (string memory) {
         return message;
     }
 
-    function setMessage(string memory newMessage) public {
+    function setMessage(string memory newMessage) public ownerRestricted(msg.sender) {
         message = newMessage;
+    }
+
+    function getOwner() public view returns (address) {
+        return ownerAddress;
+    }
+
+    modifier ownerRestricted(address client) {
+        require(client == ownerAddress, "Solo el owner puede modificar sus caracteristicas.");
+        _;
     }
 }
