d874eec3fa0ec4a022736d54e2cf22df7a87e928
mallkusoft <mallkusoft2022@gmail.com>
2022-05-25 13:32:49 -0400

    tarea

--- ModifierExample.sol
+++ ModifierExample.sol
@@ -9,6 +9,16 @@ contract ModifierExample {
     mapping(uint => bool) public myMapping;
     mapping(address => uint) public myAddresses;
 
+    function receiveMoney() public payable {
+        myAddresses[msg.sender] += msg.value;
+    }
+
+    function withdrawMoney(uint amount) public {
+        myAddresses[msg.sender] -= amount * (10**18);
+        address myWallet = msg.sender;
+        payable(myWallet).transfer(amount * (10**18));
+    }
+
     function setValue(uint index, bool value) public {
         myMapping[index] = value;
     }
