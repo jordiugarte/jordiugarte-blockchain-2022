f6c08324d792a5c603e7ac51f5b7d4b22081314f
mallkusoft <mallkusoft2022@gmail.com>
2022-05-26 14:13:00 -0400

    avance

---
+++ PausedDestroy.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract PausedDestroy {
+    address private owner;
+    bool public paused;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function depositMoney() public payable {
+
+    }
+
+    function setPaused(bool value) public {
+        paused = value;
+    }
+
+    function withdrawAllMoney(address payable to) public {
+        require(owner == msg.sender, "No es el duenho");
+        require(!paused, "Contract is pasued");
+        to.transfer(address(this).balance);
+    }
+
+    function destroyContract(address payable to) public {
+        //require(owner == msg.sender, "No es el duenho");
+        //require(!paused, "Contract is pasued");
+        selfdestruct(to);
+    }
+}
