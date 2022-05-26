3aa7aff1b0243e1f7b1dc41d3f4fab2ff70f66c7
mallkusoft <mallkusoft2022@gmail.com>
2022-05-26 13:39:45 -0400

    avance

--- ModifierExample.sol
+++ ModifierExample.sol
@@ -1,41 +1,40 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-contract ModifierExample {
-    bool public myBoolean;
-    string public myString;
-    uint public myNumber;
-
-    mapping(uint => bool) public myMapping;
-    mapping(address => uint) public myAddresses;
-
-    function receiveMoney() public payable {
-        myAddresses[msg.sender] += msg.value;
+contract StructTest {
+    struct Book {
+        string title;
+        string author;
+        uint id;
+        bool available;
+        uint quantity;
     }
 
-    function withdrawMoney(uint amount) public {
-        uint amountToEther = amount * (10 ** 18);
-        require (amountToEther <= myAddresses[msg.sender], "Not enough money");
-        myAddresses[msg.sender] -= amount * (10**18);
-        address myWallet = msg.sender;
-        payable(myWallet).transfer(amount * (10**18));
+    Book public book1;
+    Book public book2 = Book("title2", "ricardo", 2, false, 10);
 
-        //  if (amountToEther <= myAddresses[msg.sender]) {
-        //      myAddresses[msg.sender] -= amount * (10**18);
-        //      address myWallet = msg.sender;
-        //      payable(myWallet).transfer(amount * (10**18));
-        //  }
+    mapping(uint => Book) libraryBook;
+
+    function borrowBook(uint idBook) public {
+        libraryBook[idBook].quantity--;
     }
 
-    function setValue(uint index, bool value) public {
-        myMapping[index] = value;
+    function getBook(uint idBook) public view returns(bool) {
+        return libraryBook[idBook].available;
     }
 
-    function setMyAddresses(address wallet, uint amount) public {
-        myAddresses[wallet] = amount;
+    function addBook(Book memory newBook) public {
+        libraryBook[newBook.id] = newBook;
+    } 
+
+    function getTitle() public view returns(string memory) {
+        return book1.title;
     }
 
-    function getBalance() public view returns(uint) {
-        return address(this).balance;
+    function setTitle() public {
+        book1.title = "title";
+    }
+    function getTitleAndIdBook2() public view returns(string memory, uint){
+        return (book2.title, book2.id);
     }
 }
