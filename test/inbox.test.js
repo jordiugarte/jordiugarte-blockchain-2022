d23d32c974c948489a0d5fd0b11b9f351affd10f
Jordi Ugarte <jordi933@gmail.com>
2022-05-31 13:32:26 -0400

    Update inbox.test.js

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -6,4 +6,20 @@ contract('Inbox', accounts => {
         const message = await instance.getMessage.call();
         assert.equal(message, 'Hi');
     })
+
+    it('setMessage', async () => {
+        const instance = await Inbox.deployed();
+        await instance.setMessage("Hi Ricardo", {from: accounts[0]});
+        const message = await instance.getMessage.call();
+        assert.equal(message, 'Hi Ricardo');
+    })
+
+    it('setMessage should not change var message', async () => {
+        try {
+            const instance = await Inbox.deployed();
+            await instance.setMessage("just owner", {from: accounts[1]});
+        } catch (e) {
+            assert.equal(e.reason, "Solo el owner puede modificar sus caracterisitcas");
+        }
+    })
 })
