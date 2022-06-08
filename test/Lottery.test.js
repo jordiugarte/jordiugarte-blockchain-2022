d67635fe5249d300eac633e6b3a97a1d6edc3bef
mallkusoft <mallkusoft2022@gmail.com>
2022-06-08 12:41:17 -0400

    multiple accounts test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,11 +1,27 @@
 const Lottery = artifacts.require('Lottery');
 
 contract('Lottery', accounts => {
-    it("allows", async () => {
+    let instance;
+    beforeEach("deploy", async () => {
+        instance = await Lottery.new();
+    })
+
+    it("allows one account to enter", async () => {
         const instance = await Lottery.deployed();
         await instance.enter({from: accounts[1], value: web3.utils.toWei("3", "ether")});
         const players = await instance.getPlayers.call();
         assert.equal(accounts[1], players[0]);
         assert.equal(1, players.length);
+    })
+
+    it("allows multiple accounts to enter", async () => {
+        await instance.enter({from: accounts[0], value: web3.utils.toWei("3", "ether")});
+        await instance.enter({from: accounts[1], value: web3.utils.toWei("3", "ether")});
+        await instance.enter({from: accounts[2], value: web3.utils.toWei("3", "ether")});
+        const players = await instance.getPlayers.call();
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[1]);
+        assert.equal(accounts[2], players[2]);
+        assert.equal(3, players.length);
     })
 })
