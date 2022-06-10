476c5050cde9960ba13add2df7b50484d5c9e77f
mallkusoft <mallkusoft2022@gmail.com>
2022-06-10 14:05:25 -0400

    Web3 and metamask verifier

--- turbo-coin/src/App.tsx
+++ turbo-coin/src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {connectWallet, initialize} from './ethereum/web3';
 
 function App() {
+
+  useEffect(() => {
+    // @ts-ignore
+    if(window.web3) {
+      initialize();
+    }
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +27,7 @@ function App() {
         >
           Hi React, Truffle and Firebase
         </a>
+        <button onClick={() => connectWallet()}>Connect</button>
       </header>
     </div>
   );
